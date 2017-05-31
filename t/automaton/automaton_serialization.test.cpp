7a76ee3f8a10492b64e1a11efc63facb62af4838
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2017-05-31 17:28:02 +0300

    Adds tests for automaton serialization.

--- t/automaton/automaton_serialization.test.cpp
+++ t/automaton/automaton_serialization.test.cpp
@@ -5,14 +5,36 @@
 #include "../../lib/Automaton.h"
 
 
+void TestSerializingALinearAutomaton() {
+  Automaton a(Match, '\0');
+  T::okay(a.ToSerial() == "(Match)",
+      "Serializes a match");
+
+  Automaton b(Character, 'a');
+  T::okay(b.ToSerial() == "(Character 'a')",
+      "Serializes a character");
+  b.next = std::make_shared<Automaton>(Character, 'b');
+  T::okay(b.ToSerial() == "(Character 'a' (Character 'b'))",
+      "Serializes two characters");
+  b.next->next = std::make_shared<Automaton>(Match, '\0');
+  T::okay(b.ToSerial() == "(Character 'a' (Character 'b' (Match)))",
+      "Serializes two characters followed by a match");
+  b.next->next->next = std::make_shared<Automaton>(Character, 'c');
+  T::okay(b.ToSerial() == "(Character 'a' (Character 'b' (Match)))",
+      "Ignores Character after a Match.");
+}
+
+void TestSerializingASplitAutomaton() {
+}
+
 int main() {
   std::cout << "Testing automaton serialization\n";
 
-  std::cout << "  Creation from serial string\n";
-
   std::cout << "  Saving to serial string\n";
+  TestSerializingALinearAutomaton();
+  TestSerializingASplitAutomaton();
 
-  std::cout << "  Displaying a human-readable string\n";
+  std::cout << "  Creation from serial string\n";
 
   return 0;
 }
